88dc967183a764277c3bf4b012a6bf04e930e207
Alan.Grolleau <alan.grolleau@gmail.com>
2025-01-27 02:45:34 +0100

    Ajout de l'affichage du personnel et de l'zafficjage de l'image dans les nouveaux agents

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -1,7 +1,36 @@
+import { useEffect,useState } from 'react'
+import styles from './gallery.module.scss'
 function Gallery(){
+
+    console.log('Chargement composant galerie')
+    const [datas,setDatas]=useState([])
+    
+    const getDatas=async()=>{
+        const response=await fetch('http://localhost:4000/techs')
+        const data=await response.json()
+        data!==datas?setDatas(data):datas
+    }  
+
+    useEffect(()=>{getDatas()},[])
+
+    console.log(datas)
     return(
-        <div>
-            Gallery
+        <div className={styles.gallery}>
+            <h2>Galerie du personnel</h2>
+            <ul className={styles.gallery__items}>
+                {datas.map(data=>{
+                    return(
+                    <li key={data._id} className={styles.item}>
+                        <img className={styles.item__picture} src={data.imageUrl} alt="" />
+                        <div className={styles.item__infos}>
+                            <h3>{data.nom}</h3>
+                            <p>{data.prenom}</p>
+                            <p>{data._id}</p>
+                        </div>
+                    </li>)
+                })}
+
+            </ul>
         </div>
     )
 }
--- src/components/nouveauTech/index.jsx
+++ src/components/nouveauTech/index.jsx
@@ -1,15 +1,78 @@
 import styles from './nouveauTech.module.scss'
-function NouveauTech(){
-    const configTech=['nom','prenom']
-    console.log(configTech)
+import { useEffect, useState } from 'react'
 
+
+function NouveauTech(){
+    const formData=new FormData()
+    const [infos,setInfos]=useState({
+        nom:'',
+        prenom:'',
+        birthDate:'',
+        nSecu:'',
+        nationality:'',
+        imageUrl:''
+    })
+    const [file,setFile]=useState(null)
+    const [imgSrc,setImgSrc]=useState(null)
+
+    const getResponse=async()=>{
+        const keys=Object.keys(infos)
+        keys.map((key)=>formData.append(key,infos[key]))
+        formData.append('file',file)
+        try{console.log (JSON.stringify(formData))
+            const response= await fetch('http://localhost:4000/techs/newtech',{
+                method:'POST',
+                body:formData
+            })
+                if(!response.ok){
+                    console.log('Pas de réponse')
+                    throw new Error(`Problème réponse nok: ${response.status}`)
+                }
+            const data=await response.json()
+            console.log(data)           
+        }catch(error){
+            console.error(error)
+        }
+    }
+    
+const handleFileChange=(e)=>{
+    const selectedFile=e.target.files[0]
+    if(selectedFile){
+        setFile(selectedFile)
+        const reader=new FileReader()
+        reader.onload=()=>{
+            setImgSrc(reader.result)
+        }
+        reader.readAsDataURL(selectedFile)
+    }
+}
+
+
+
+    
     return(
         <div> 
             <h2>Nouveau tech </h2>
+            <div classname={styles.pictureContainer}>
+            <img className={styles.picture} src={imgSrc} alt="" /></div>
             <div className={styles.form}>
-                <input type="text" placeholder="nom" />
-                <input type="text" placeholder="prenom"/>
-                <button>Valider</button>
+                <input className={styles.form__input} type="text" placeholder="nom" onChange={(e)=>{
+                    setInfos({...infos,nom:e.target.value})
+                    }}/>
+                <input className={styles.form__input} type="text" placeholder="prenom" onChange={(e)=>{
+                    setInfos({...infos,prenom:e.target.value})
+                }}/>
+                <input className={styles.form__input} type="date" placeholder="Date de naissance" onChange={(e)=>{
+                    setInfos({...infos,birthDate:e.target.value})
+                }}/>
+                <input className={styles.form__input} type="text" placeholder="Numéro sécu" onChange={(e)=>{
+                    setInfos({...infos,nSecu:e.target.value})
+                }}/>
+                <input className={styles.form__input} type="text" placeholder="Nationalité" onChange={(e)=>{
+                    setInfos({...infos,nationality:e.target.value})
+                }}/>
+                <input className={styles.form__inputFile} type="file" onChange={handleFileChange}/>
+                <button type='submit' onClick={getResponse}>Valider</button>
             </div>
         </div>
     )
